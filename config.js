fd57d7ebbc3c76357eac3dd9fb78f625ce43159f
AndyHJay83 <120287095+AndyHJay83@users.noreply.github.com>
2025-03-22 19:07:23 +0000

    Delete config.js

--- config.js
+++
@@ -1,3 +0,0 @@
-const config = {
-    OPENAI_API_KEY: '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'
-}; 
